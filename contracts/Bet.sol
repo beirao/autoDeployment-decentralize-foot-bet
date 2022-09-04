143c8aba5915635e5335827b32e48e7814834883
Toma <thomasmarques911@gmail.com>
2022-09-04 07:27:07 +0200

    clean the chainlink source folder

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.16;
 import "../chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "../chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 import "../chainlink/contracts/src/v0.8/KeeperCompatible.sol";
-
+//esed
 // Errors
 error Bet__UpkeepNotNeeded(uint256 currentBalance, uint256 betState);
 error Bet__betValueNotCorrect(uint256 betState);
